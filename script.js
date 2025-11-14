9dddcaa712b68439c2e72be638e5dfd54b595c5e
Sumit Kumar <sumitkarn12@gmail.com>
2025-11-15 04:07:56 +0530

    - FA Icons added - Bulk rename feature added - Card actions becomes non-functional upon selection - Memory relases upon recording deletion - JS & CSS code optimization - Controls buttons updated - DOM updating instead of rerendering after removing, renaming in bulk or single

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const pauseBtn = document.getElementById('pause-btn');
 const resumeBtn = document.getElementById('resume-btn');
 const stopBtn = document.getElementById('stop-btn');
 const controlsWrapper = document.querySelector('.controls-wrapper');
+const recorderEl = document.querySelector('#main-content .recorder');
 const msg = document.getElementById('msg');
 const videoPlayback = document.getElementById('video-playback');
 const historyContainer = document.getElementById('recordings');
 const cancelRecordingCardSelection = document.getElementById('cancel-recording-selection');
 const modal = document.querySelector('#playback-modal');
 const multiDeleteBtn = document.querySelector('#delete-multiple-button');
-const template = Handlebars.compile( document.querySelector('#recording-card-template').innerHTML );
+const multiRenameBtn = document.querySelector('#rename-multiple-button');
+const template = Handlebars.compile(document.querySelector('#recording-card-template').innerHTML);
 const parser = new DOMParser();
 
-alertify.defaults.glossary.title = "👋";
+alertify.defaults.glossary.title = "🖥️";
 alertify.defaults.notifier.closeButton = true;
 alertify.set("notifier", "position", "top-center");
 
 const HISTORY_KEY = `RECORDINGS`;
+const VIDEO_EXT = `webm`;
 const DEF_OF_NEW_RECORDING = 5 * 60 * 1000; // Not recorded more than 5 mins ago 
 
 let mediaRecorder, stream;
@@ -28,18 +31,30 @@ db.version(1).stores({
   recordings: `++id, title, blob, at`,
 });
 
+async function estimateStorage() {
+  let details = "Storage Estimation API Not Supported.";
+  if ('storage' in navigator && 'estimate' in navigator.storage) {
+    const { usage, quota } = await navigator.storage.estimate();
+    const percentUsed = (usage / quota * 100).toFixed(2);
+    const usageInMb = ((usage / (1024 * 1024))).toFixed(2);
+    const quotaInMb = ((quota / (1024 * 1024))).toFixed(2);
+    details = `${usageInMb} out of ${quotaInMb} MB used (${percentUsed}%)`;
+  }
+  return details;
+}
+
 const dateFormatter = new Intl.DateTimeFormat(navigator.language, {
   dateStyle: "medium",
   timeStyle: "short"
 });
 
 function updateControls(start_shown = 1, pause_shown = 0, resume_shown = 0, stop_shown = 0) {
-  startBtn.style.display = (start_shown) ? "block" : "none";
-  pauseBtn.style.display = (pause_shown) ? "block" : "none";
-  resumeBtn.style.display = (resume_shown) ? "block" : "none";
-  stopBtn.style.display = (stop_shown) ? "block" : "none";
-
-  controlsWrapper.style.height = stop_shown?"100%": "auto";
+  timer.el.style.display = (!start_shown) ? "block" : "none";
+  startBtn.style.display = start_shown ? "block" : "none";
+  pauseBtn.style.display = pause_shown ? "block" : "none";
+  resumeBtn.style.display = resume_shown ? "block" : "none";
+  stopBtn.style.display = stop_shown ? "block" : "none";
+  recorderEl.style.height = stop_shown ? "100dvh" : "auto";
 }
 
 function createElement(nodeName, classes, textContent) {
@@ -47,7 +62,7 @@ function createElement(nodeName, classes, textContent) {
   classes.forEach(c => {
     el.classList.add(c);
   });
-  el.textContent = textContent;
+  el.innerHTML = textContent;
   return el;
 }
 
@@ -60,17 +75,18 @@ function saveRecording(mimeType) {
 
   let v = document.createElement("video");
   v.addEventListener("loadeddata", ev => {
-    console.log( "LOADEDDATA", ev );
+    console.log("LOADEDDATA", ev);
     let recordedAt = new Date();
     db.recordings.add({
       "blob": blob,
       "at": recordedAt,
-      "title": `record.idkey.in-${dateFormatter.format(new Date()).replaceAll(/\W/gi, '-')}.webm`,
+      "title": `record.idkey.in-${dateFormatter.format(new Date()).replaceAll(/\W/gi, '-')}.${VIDEO_EXT}`,
       "duration": Math.round(v.duration),
-      "dimension": v.videoWidth+"x"+v.videoHeight
+      "dimension": v.videoWidth + "x" + v.videoHeight
     }).then(a => {
       renderHistory();
       v.remove();
+      URL.revokeObjectURL( videoURL );
     });
     // Stop the media stream tracks
     stream.getTracks().forEach(track => track.stop());
@@ -80,54 +96,94 @@ function saveRecording(mimeType) {
 }
 
 function closeModal() {
-  document.querySelectorAll(".modal").forEach( el => el.classList.remove("is-active"));
+  document.querySelectorAll(".modal").forEach(el => el.classList.remove("is-active"));
   videoPlayback.pause();
 }
 
-function cancelRecordingCardSelector() {
-  document.querySelectorAll(".recording-card-selector").forEach(el => {
-    el.checked = false;
+function rename( keys, callback ) {
+  db.recordings.orderBy("id").filter(r => keys.includes(r.id)).toArray().then( recs => {
+    if (recs.length == 0) throw new Error("Nothing to be renamed");
+    alertify.prompt(`Enter new name`, (recs.length == 1)?recs[0].title.replace( "."+VIDEO_EXT, ""):"", async function (evt, p) {
+      let renamedRecs = recs.map((r, i) => {
+        p = p.replaceAll(/\W/gi, "-").replaceAll(/-{2,}/gi, "-");
+        p = p.substring(0, 128);
+        r.title = p + ((recs.length > 1)?` - ${i}`:"")+`.${VIDEO_EXT}`;
+        return r;
+      });
+      db.recordings.bulkPut(renamedRecs).then(r => {
+        alertify.success(`Renamed ${renamedRecs.length} recording${(renamedRecs.length > 1) ? "s" : ""}.`);
+        callback( renamedRecs );
+      });
+    });
+  });
+}
+
+function remove( keys, callback ) {
+  alertify.confirm(`Are your sure to delete ${keys.length} recording${(keys.length > 1) ? "s" : ""}?`, async function () {
+    db.recordings.bulkDelete( keys ).then(r => {
+      alertify.success(`Deleted ${keys.length} recording${(keys.length > 1) ? "s" : ""}.`);
+      callback( keys );
+    });
   });
 }
 
 videoPlayback.addEventListener("resize", e => {
-  console.log( "Video Dimensio", e.target.videoWidth, e.target.videoHeight );
+  console.log("Video Dimensio", e.target.videoWidth, e.target.videoHeight);
   modal.querySelector(".modal-content").style.height = "auto";
   modal.querySelector(".modal-content").style.width = "auto";
-  if ( e.target.videoWidth - e.target.videoHeight) {
+  if (e.target.videoWidth - e.target.videoHeight) {
     modal.querySelector(".modal-content").style.height = "100%";
   } else {
     modal.querySelector(".modal-content").style.height = "100%";
   }
   let isDirty = false;
-  if ( !currentRecording.dimension ) {
+  if (!currentRecording.dimension) {
     currentRecording.dimension = `${e.target.videoWidth}x${e.target.videoHeight}`
     isDirty = true;
   }
-  if ( !currentRecording.duration ) {
+  if (!currentRecording.duration) {
     currentRecording.duration = Math.round(e.target.duration);
     isDirty = true;
   }
-  if ( isDirty )
-  db.recordings.put( currentRecording ).then( r => {
-    console.log( r, "Record updated" )
-  });
+  if (isDirty)
+    db.recordings.put(currentRecording).then(r => {
+      console.log(r, "Record updated")
+    });
 })
 
-cancelRecordingCardSelection.addEventListener("click", cancelRecordingCardSelector);
+cancelRecordingCardSelection.addEventListener("click", e=> {
+  document.querySelectorAll(".recording-card-selector").forEach(el => {
+    el.checked = false;
+  });
+});
 
 document.querySelectorAll('.modal-close,.modal-background').forEach(el => {
   el.addEventListener("click", closeModal);
 });
 
+multiRenameBtn.addEventListener("click", async e => {
+  let selectedContent = document.querySelectorAll("#recordings input[type=checkbox]:checked");
+  let keys = Array.from(selectedContent).map(c => Number(c.value));
+  rename(keys, recs => {
+    recs.forEach(r => {
+      const title = document.querySelector(`label[data-id='${r.id}'] .rec-title`);
+      title.textContent = r.title;
+    });
+    cancelRecordingCardSelection.click();
+  });
+});
+
 multiDeleteBtn.addEventListener("click", async e => {
   let selectedContent = document.querySelectorAll("input[type=checkbox]:checked");
   let keys = Array.from(selectedContent).map(c => Number(c.value));
-  if ( keys.length == 0 ) return;
-  alertify.confirm(`Are your sure to delete ${keys.length} recording${(keys.length>1)?"s":""}?`,async function() {
-    await db.recordings.bulkDelete(keys).then( r=> {
-      alertify.success(`Deleted ${keys.length} recording${(keys.length > 1) ? "s" : ""}.` );
-      renderHistory();
+  if (keys.length == 0) return;
+
+  remove(keys, ids => {
+    ids.forEach( k => {
+      const card = document.querySelector(`label[data-id='${k}']`);
+      URL.revokeObjectURL(card.querySelector(".download").href);
+      card.style.transform = "scale(0)";
+      setTimeout(() => card.remove(), 500);
     });
   });
 });
@@ -138,7 +194,7 @@ startBtn.addEventListener('click', async () => {
     stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
 
     // Set up the MediaRecorder
-    mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm; codecs=vp9' });
+    mediaRecorder = new MediaRecorder(stream, { mimeType: `video/${VIDEO_EXT}; codecs=vp9` });
 
     // Listen for data chunks and push them into the array
     mediaRecorder.ondataavailable = (event) => {
@@ -157,7 +213,7 @@ startBtn.addEventListener('click', async () => {
     timer.start();
     timer.el.style.display = "block";
   } catch (err) {
-    alertify.error( `Couldn't start recording.\n${err.message}` );
+    alertify.error(`Couldn't start recording.\n${err.message}`);
     updateControls(1, 0, 0, 0);
   }
 });
@@ -174,7 +230,7 @@ resumeBtn.addEventListener('click', () => {
   if (mediaRecorder && mediaRecorder.state == 'paused') {
     mediaRecorder.resume();
     updateControls(0, 1, 0, 1);
-    timer.start( false );
+    timer.start(false);
   }
 });
 
@@ -185,41 +241,31 @@ stopBtn.addEventListener('click', () => {
   // stopping & hiding timer code is in the saveRecording function
 });
 
-historyContainer.addEventListener( "click", e => {
-  if ( e.target.classList.contains("del")  ) {
-    videoPlayback.src = null;
-    alertify.confirm("Are you sure to delete this?", async () => {
-      await db.recordings.delete( Number(e.target.dataset.id) );
-      alertify.success("Deleted!");
-      const card = document.querySelector(`label[data-id='${e.target.dataset.id}']`);
-      card.style.transform = "scale(0)";
-      setTimeout(()=> card.remove(), 500);
-    });
-  } else if ( e.target.classList.contains("rename")  ) {
-    db.recordings.get(Number(e.target.dataset.id)).then( data => {
-      let ts = data.title.split(".");
-      let t = data.title.replace("." + ts[ts.length - 1], "");
+historyContainer.addEventListener("click", e => {
+  let button = e.target.closest(".card-footer-item");
+  if ( !button ) return;
 
-      alertify.prompt("Enter new name", t, async function (evt, p) {
-        if (p.trim().length == 0) {
-          alertify.error("Title can't be empty.");
-        } else {
-          p = p.replaceAll(/\W/gi, "-").replaceAll(/-{2,}/gi, "-");
-          p = p.substring(0, 128);
-          p = p + "." + ts[ts.length - 1];
-          await db.recordings.update(data.id, { title: p });
-          alertify.success('Renamed to ' + p);
-          const title = document.querySelector(`label[data-id='${e.target.dataset.id}'] .rec-title` );
-          title.textContent = p;
-        }
+  if (button.classList.contains("del")) {
+    videoPlayback.src = null;
+    remove([Number(button.dataset.id)], keys => {
+      keys.forEach( k => {
+        const card = document.querySelector(`label[data-id='${k}']`);
+        URL.revokeObjectURL(card.querySelector(".download").href);
+        card.style.transform = "scale(0)";
+        setTimeout(() => card.remove(), 500);
       });
     });
-
-
-  } else if ( e.target.classList.contains("preview")  ) {
-    db.recordings.get( Number(e.target.dataset.id) ).then( data => {
+  } else if (button.classList.contains("rename")) {
+    rename([Number(button.dataset.id)], recs => {
+      recs.forEach( r => {
+        const title = document.querySelector(`label[data-id='${r.id}'] .rec-title`);
+        title.textContent = r.title;
+      });
+    });
+  } else if (button.classList.contains("preview")) {
+    db.recordings.get(Number(button.dataset.id)).then(data => {
       currentRecording = data;
-      videoPlayback.src = document.querySelector(`.download[data-id='${e.target.dataset.id}']`).href;
+      videoPlayback.src = document.querySelector(`.download[data-id='${button.dataset.id}']`).href;
       modal.classList.add("is-active");
     });
   }
@@ -228,54 +274,41 @@ historyContainer.addEventListener( "click", e => {
 document.addEventListener("keydown", e => {
   // List of tag names to exclude
   const excludedTags = ['INPUT', 'TEXTAREA', 'SELECT'];
-  if (excludedTags.includes(e.target.tagName)) {
-    // If the event originated from an excluded input field, do nothing.
-    return;
-  }
+  // If the event originated from an excluded input field, do nothing.
+  if (excludedTags.includes(e.target.tagName))  return;
+
+  const shortcuts = [
+    { keys: '?', description: 'Show all keyboard shortcuts (this popup)' },
+    { keys: 'S', description: 'Start/Stop recording' },
+    { keys: 'P', description: 'Pause recording' },
+    { keys: 'R', description: 'Resume recording' },
+    { keys: 'Ctrl + A', description: 'Select all recording cards' },
+    { keys: 'Backspace', description: 'Delete selected recording cards' },
+    { keys: 'Esc', description: 'Close any active modal / Cancel card selection' }
+  ];
 
   e.preventDefault();
 
-  if (e.ctrlKey)
-    console.log(e.keyCode, e.key);
+  const pressedKey = String(e.key).toLowerCase();
+  const mediaState = mediaRecorder?.state || "inactive";
 
-  if (e.key === '?' && e.shiftKey) {
+  console.log( pressedKey, mediaState );
+
+  if ( pressedKey === '?' ) {
     document.getElementById('shortcuts-modal').classList.add("is-active");
-    const shortcuts = [
-      { keys: 'Shift + ?', description: 'Show all keyboard shortcuts (this popup)' },
-      { keys: 'S', description: 'Start/Stop recording' },
-      { keys: 'P', description: 'Pause recording' },
-      { keys: 'R', description: 'Resume recording' },
-      { keys: 'Ctrl + A', description: 'Select all recording cards' },
-      { keys: 'Backspace', description: 'Delete selected recording cards' },
-      { keys: 'Esc', description: 'Close any active modal / Cancel card selection' }
-    ];
-  }
-
-
-  if (mediaRecorder) {
-    if (e.keyCode == 83 && mediaRecorder.state == "inactive") {
-      startBtn.click();
-    } else if (e.keyCode == 83 && mediaRecorder.state == "recording") {
-      stopBtn.click();
-    } else if (e.keyCode == 80 && mediaRecorder.state == "recording") {
-      pauseBtn.click();
-    } else if (e.keyCode == 82 && mediaRecorder.state == "paused") {
-      resumeBtn.click();
-    }
-  } else if (e.keyCode == 83) {
-    startBtn.click();
-  } else if (e.ctrlKey && e.keyCode == 65) {
+  } else  if (pressedKey == 's' && mediaState == "inactive") { startBtn.click(); }
+  else if (pressedKey == 's' && mediaState == "recording") { stopBtn.click(); }
+  else if (pressedKey =="p" && mediaState == "recording") { pauseBtn.click(); }
+  else if (pressedKey == "r" && mediaState == "paused") { resumeBtn.click(); }
+  else if ((e.ctrlKey || e.metaKey) && pressedKey == 'a') {
     document.querySelectorAll(".recording-card-selector").forEach(el => {
       el.checked = true;
     });
-  } else if (e.keyCode == 8) {
-    multiDeleteBtn.click();
-  }
-
-  if (e.keyCode == 27) {
+  } else if (pressedKey == "backspace" || pressedKey == "delete") { multiDeleteBtn.click(); }
+  else if (pressedKey == "r") { multiRenameBtn.click(); }
+  else if (pressedKey == 'escape') {
     closeModal();
-    cancelRecordingCardSelector();
-    closeShortcutsModal();
+    cancelRecordingCardSelection.click();
   }
 });
 
@@ -285,66 +318,71 @@ function renderHistory() {
 
   db.recordings.orderBy("at").reverse().each(data => {
     data.poster = `https://picsum.photos/seed/${data.id}/900/300.webp`;
-    data.size = (data.blob.size / (1024 * 1024)).toFixed(2)+"MB";
+    data.size = (data.blob.size / (1024 * 1024)).toFixed(2) + "MB";
     data.time = data.at.toLocaleTimeString(navigator.language);
     let d = new Date(data.duration * 1000);
     data.durationAsTime = d.toJSON().substring(11, 19)
     if (current_date != data.at.toDateString()) {
       current_date = data.at.toDateString();
-      let li = createElement("div", ["category", "column", "is-full"], `🗓️ ${current_date}`);
+      let li = createElement("div", ["category", "column", "is-full"], `<span class="icon"><i class="fa fa-calendar"></i></span> <span>${current_date}</span>`);
       historyContainer.appendChild(li);
     }
 
     let node = parser.parseFromString(template(data), "text/html");
     node = node.querySelector("label");
-    node.querySelector(".download").href = URL.createObjectURL( data.blob );
+    node.querySelector(".download").href = URL.createObjectURL(data.blob);
 
-    if ( (Date.now() - data.at.getTime()) < DEF_OF_NEW_RECORDING ) {
+    if ((Date.now() - data.at.getTime()) < DEF_OF_NEW_RECORDING) {
       let newTag = createElement("div", ["new-tag"], null);
-      node.querySelector(".card-content").appendChild( newTag );
+      node.querySelector(".card-content").appendChild(newTag);
     }
 
-    historyContainer.appendChild( node );
+    node.querySelector("img").onload = (ev) => {
+      ev.target.parentNode.classList.remove("is-skeleton");
+    }
+
+    historyContainer.appendChild(node);
   });
 
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 const timer = {
-  el: document.querySelector(".controls-wrapper .timer"),
+  el: document.querySelector("#main-content .timer"),
   t: null,
   s: 0,
-  start: function( reset = true ) {
-    if ( reset ) this.s = 0;
-    clearInterval( this.t )
-    this.t = setInterval( () => {
+  start: function (reset = true) {
+    if (reset) this.s = 0;
+    clearInterval(this.t)
+    this.t = setInterval(() => {
       this.el.textContent = (new Date(++this.s * 1000)).toJSON().substring(11, 19)
     }, 1000);
   },
-  stop: function() {
-    clearInterval( this.t );
+  stop: function () {
+    clearInterval(this.t);
   }
 }
 
 renderHistory();
 
 if (!navigator.mediaDevices.getDisplayMedia) {
-  document.querySelector(".controls").style.display = "none";
   document.querySelector("#main-content").style.display = "none";
   msg.innerText = "Your browser does not support screen recording feature.";
 } else {
-  setInterval( async () => {
+  // Auto refresh available storage
+  setInterval(async () => {
     msg.style.display = "block";
-    let details = "Storage Estimation API Not Supported";
-    if ('storage' in navigator && 'estimate' in navigator.storage) {
-      const { usage, quota } = await navigator.storage.estimate();
-      const percentUsed = (usage / quota * 100).toFixed(2);
-      const usageInMb = ((usage / (1024 * 1024))).toFixed(2);
-      const quotaInMb = ((quota / (1024 * 1024))).toFixed(2);
-      details = `${usageInMb} out of ${quotaInMb} MB used (${percentUsed}%)`;
+    let m = await estimateStorage()
+    if ( msg.textContent != m ) {
+      // alertify.message( `Available storage updated.` );
+      msg.textContent = m;
     }
-    msg.textContent = details;
-  }, 10);
+    // console.log( "Available storage checked!" );
+  }, 10*1000);
+  estimateStorage().then( m => {
+    msg.style.display = "block";
+    msg.textContent = m;
+  });
 }
 
 if (typeof navigator.serviceWorker !== 'undefined') {
